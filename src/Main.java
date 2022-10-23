b4246f3704c8eda40e196060b9434ce6681f56b0
FelipeOthon <felipeosantiago@gmail.com>
2022-10-23 14:17:13 -0300

    enviar exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a, b, c, d, dif;
+
+        System.out.println("Digite o valor de A");
+        a = scanner.nextInt();
+
+        System.out.println("Digite o valor de B");
+        b = scanner.nextInt();
+
+        System.out.println("Digite o valor de C");
+        c = scanner.nextInt();
+
+        System.out.println("Digite o valor de D");
+        d = scanner.nextInt();
+
+        dif = a * b - c * d;
+
+        System.out.println("A diferença é: " + dif);
+
+        scanner.close();
     }
 }
